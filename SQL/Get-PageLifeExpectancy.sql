3287970d2202b8e15584b77ce26894de6aa12dbc
mattselkey <matthewselkirk@gmail.com>
2019-10-18 15:30:52 +0200

    SQL LPE T-SQL

--- SQL/Get-PageLifeExpectancy.sql
+++ SQL/Get-PageLifeExpectancy.sql
@@ -1,3 +1,5 @@
+/* -- https://sqlperformance.com/2014/10/sql-performance/knee-jerk-page-life-expectancy -- */
+
 SELECT [object_name],
 [counter_name],
 [cntr_value] FROM sys.dm_os_performance_counters
