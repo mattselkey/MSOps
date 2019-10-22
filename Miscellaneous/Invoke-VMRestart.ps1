0c10bdb825ebb3d4b192897edb75d1609b76bad8
mattselkey <matthewselkirk@gmail.com>
2019-10-22 14:19:26 +0200

    vm restart template

---
+++ Miscellaneous/Invoke-VMRestart.ps1
@@ -0,0 +1,46 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $ComputerName,
+    [Boolean]
+    $Now=$true
+)
+
+$credentials = Get-Credential
+
+try{
+if($Now){
+        Restart-Computer -ComputerName $ComputerName -Credential $credentials -Force
+}else{
+        Restart-Computer -ComputerName $ComputerName -Credential $credentials
+}
+catch{
+
+
+
+}
+
+try{
+Test-Connection $ComputerName -Count 20 -Delay 1
+}
+catch{
+
+    Write-Information Rebooted
+    
+}
