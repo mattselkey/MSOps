7f7bcbb6e895c55264fe40e4fbeb32e7623d3f1d
mattselkey <matthewselkirk@gmail.com>
2019-10-24 16:49:21 +0200

    new check for stopped services script

---
+++ Miscellaneous/Get-StoppedAutoServices.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    Coonect to a remote server, check fors stopped services.
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $Computername
+)
+
+$credentials = Get-Credential
+
+
+Invoke-Command -ComputerName $Computername  -Credential $credentials -scriptblock {
+
+
+    Get-Service | Where-Object {($_.Status -eq "Stopped") -and ($_.StartType -eq "Automatic")}
+
+}
