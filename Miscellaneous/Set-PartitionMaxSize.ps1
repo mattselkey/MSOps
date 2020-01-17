d84bab12ef7a4b83a17f98a5e654a6fe85d55bc3
mattselkey <matthewselkirk@gmail.com>
2020-01-17 15:15:44 +0100

    updates

--- Miscellaneous/Set-PartitionMaxSize.ps1
+++ Miscellaneous/Set-PartitionMaxSize.ps1
@@ -15,15 +15,62 @@
 #>
 [CmdletBinding()]
 param (
-    [Parameter()]
+    [Parameter(Mandatory=$True)]
     [String]
-    $MachineName
+    $MachineName,
+    [Parameter(Mandatory=$False)]
+    [String]
+    $DriveLetter="C",
+    [Parameter(Mandatory=$False)]
+    [boolean]$OutInfo=$True
 )
 
-$credentials = Get-Credential
-$cimSession = New-CimSession -ComputerName $MachineName -Credential $credentials
+BEGIN{
+    if($OutInfo){
+        $VerbosePreference = "Continue"
+        $InformationPreference = "Continue"
+        Write-Information "messages will be passed to output stream."
+    }else{
+        $VerbosePreference = "SilentlyContinue"
+         $InformationPreference = "SilentlyContinue"
+    }
+}
 
-$partition = Get-Partition -CimSession $cimSession | Where-Object {$_.DriveLetter  -eq "C"}
-$size  = Get-PartitionSupportedSize -CimSession $cimSession -DriveLetter C
+PROCESS{
 
-Resize-Partition -CimSession $cimSession -DiskNumber $partition.DiskNumber -PartitionNumber $partition.PartitionNumber -Size $size.SizeMax
+try{
+    Write-Information -MessageData "Requesting credentials for remote session"
+    $credentials = Get-Credential
+
+    Write-Information -MessageData "Connecting to remote machine"
+    $cimSession = New-CimSession -ComputerName $MachineName -Credential $credentials
+
+    Write-Information -MessageData "Getting partition named $($DriveLetter)"
+    $partition = Get-Partition -CimSession $cimSession | Where-Object {$_.DriveLetter  -eq $DriveLetter}
+    $currentPartitionSize = $partition.Size
+    $diskNumber = $partition.DiskNumber
+    $partitonNumber = $partition.PartitionNumber
+
+    Write-Information -MessageData "Disknumber is $($diskNumber) and Parition Number is $($partitonNumber)."
+
+    Write-Information -MessageData "Getting Partiton Supportd Size."
+    $size  = Get-PartitionSupportedSize -CimSession $cimSession -DriveLetter $DriveLetter
+
+    Write-Information -MessageData "Resizing Partititon."
+    Resize-Partition -CimSession $cimSession -DiskNumber $diskNumber -PartitionNumber $partitonNumber -Size $size.SizeMax
+
+    $updatedPartiton = Get-Partition -CimSession $credentials -DriveLetter $DriveLetter
+
+    Write-Information -MessageData "Previous partiton size was $($currentPartitionSize), New partition size is $($updatedPartiton)"
+
+}catch{
+    Write-Error "Resize failed ($_)"
+}
+
+}
+
+END {
+    Write-Information -MessageData "Closing cimsession."
+    Remove-CimSession -CimSession $cimSession 
+
+}
